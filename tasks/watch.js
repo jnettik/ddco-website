dd6232df346d0068ef30e5c7ce7681cef79c5f99
James Nettik <james@atendesigngroup.com>
2017-05-20 12:35:58 -0600

    Fixed broken things. Created a cleand up billboard component.

--- tasks/watch.js
+++ tasks/watch.js
@@ -9,7 +9,7 @@ const watch = require('gulp-watch');
 const sequence = require('run-sequence');
 
 gulp.task('watch', () => {
-  sequence('css','html','styleguide','browser-sync', () => {
+  sequence('css','html','browser-sync', () => {
     watch(config.html.paths, () => { sequence('html'); });
     watch(config.styleguide.paths, () => { sequence('styleguide'); });
     watch(config.css.paths, () => { sequence('css','html') });
